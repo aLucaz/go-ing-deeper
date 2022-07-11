e612d0614c6c63c46e613ee9f6a414c8ccd12e3d
jlucas <jlucas@proveedor.intercorp.com.pe>
2022-07-11 15:07:24 -0500

    Adding channels routines and memory example

--- intermediate/channels/checker/main.go
+++ intermediate/channels/checker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -18,16 +19,23 @@ func main() {
 		// to let the scheduler decide when to add go routines
 		go checkLink(link, c)
 	}
-	fmt.Println(<-c)
+
+	for l := range c {
+		lRefCopy := l
+		go func(link string) {
+			time.Sleep(time.Second * 5)
+			checkLink(link, c)
+		}(lRefCopy)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " is down!")
-		c <- "Link is down"
+		c <- link
 		return
 	}
 	fmt.Println(link, " is up!")
-	c <- "Link is up"
+	c <- link
 }
